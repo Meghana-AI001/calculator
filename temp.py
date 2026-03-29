8642f78a3aa6961f9d462cc88cfe7764f731b6ed
meghana <meghana84313@gmail.com>
2026-03-29 15:35:41 +0530

    ....

--- temp.py
+++
@@ -1,6 +0,0 @@
-password=input("enter password")
-isalnum=True
-if len(password)>=8 and isalnum and any(c.isupper() for c in password):
-    print("strong password")
-else:
-    print("weak passoword, please be it atleast 1 capitl ,1 special character and 1 numerical value")
